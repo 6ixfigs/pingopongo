219d314a000ec7d152abb625d2d0745f019a6924
Marcel Muslija <marcel.muslija@gmail.com>
2025-01-09 20:55:19 +0100

    Parse slack request

--- internal/rest/rest.go
+++ internal/rest/rest.go
@@ -38,5 +38,38 @@ func (s *Server) MountRoutes() {
 }
 
 func (s *Server) parse(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("OK"))
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, "Something went wrong", http.StatusOK)
+		return
+	}
+
+	request := &SlackRequest{
+		r.FormValue("team_id"),
+		r.FormValue("team_domain"),
+		r.FormValue("enterprise_id"),
+		r.FormValue("enterprise_name"),
+		r.FormValue("channel_id"),
+		r.FormValue("channel_name"),
+		r.FormValue("user_id"),
+		r.FormValue("command"),
+		r.FormValue("text"),
+		r.FormValue("response_url"),
+		r.FormValue("trigger_id"),
+		r.FormValue("api_app_id"),
+	}
+
+	switch request.command {
+	case "/leaderboard":
+		leaderboard(w, request)
+	default:
+		http.Error(w, "Received invalid command", http.StatusOK)
+	}
+}
+
+func leaderboard(w http.ResponseWriter, r *SlackRequest) {
+	// fetch all records where channel_id=r.channel_id
+	// use conversations.members API to map user_ids to user names
+	// format the leaderboard
+	// write response and make it in_channel (visible to all in channel)
+	w.Write([]byte("OK from leaderboard"))
 }
---
+++ internal/rest/types.go
@@ -0,0 +1,16 @@
+package rest
+
+type SlackRequest struct {
+	team_id         string
+	team_domain     string
+	enterprise_id   string
+	enterprise_name string
+	channel_id      string
+	channel_name    string
+	user_id         string
+	command         string
+	text            string
+	response_url    string
+	trigger_id      string
+	api_app_id      string
+}
