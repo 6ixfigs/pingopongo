e8739b11cead70cdf640843fdebbb241408122e0
Marcel Muslija <marcel.muslija@gmail.com>
2025-01-19 21:20:34 +0100

    Add missing columns, set default values

--- migrations/000001_create_players_table.up.sql
+++ migrations/000001_create_players_table.up.sql
@@ -1,13 +1,16 @@
 CREATE TABLE players (
 	id integer GENERATED ALWAYS AS IDENTITY,
 	user_id varchar(255) NOT NULL,
-	full_name varchar(255) NOT NULL,
 	channel_id varchar(255) NOT NULL,
-	matches_won integer,
-	matches_drawn integer,
-	matches_lost integer,
-	games_won integer,
-	games_lost integer,
-	current_streak integer,
-	UNIQUE(user_id, channel_id)
+	team_id varchar(255) NOT NULL
+	full_name varchar(255) NOT NULL,
+	matches_won integer DEFAULT 0,
+	matches_drawn integer DEFAULT 0,
+	matches_lost integer DEFAULT 0,
+	games_won integer DEFAULT 0,
+	games_lost integer DEFAULT 0,
+	points_won integer DEFAULT 0,
+	current_streak integer DEFAULT 0,
+	elo integer DEFAULT 1000,
+	UNIQUE(user_id, channel_id, team_id)
 );
