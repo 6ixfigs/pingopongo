d1523f8b55615e8e5f2d9dbce281cd90355c84e5
Dino Smircic <dino.smircic@fer.hr>
2025-02-01 16:18:40 +0100

    Rename 'cli' to pongo

---
+++ pongo/cmd/record.go
@@ -0,0 +1,37 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+// recordCmd represents the record command
+var recordCmd = &cobra.Command{
+	Use:   "record",
+	Short: "Records a match between two players.",
+	Long: `Sends the command containing the match recording to server.
+	For example:
+
+record [GroupName] [player1] [player2] [score]`,
+	Run: func(cmd *cobra.Command, args []string) {
+		sendCommand("record", strings.Join(args, " "))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(recordCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// recordCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// recordCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ pongo/main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+import "github.com/6ixfigs/pingypongy/pongo/cmd"
+
+func main() {
+	cmd.Execute()
+}
