553db2952ebde1c1a7cd9dcf225291a24f18f7e5
dsmircic <dino.smircic@fer.hr>
2025-02-06 19:40:47 +0100

    Remove pingypongy.go

--- cmd/server/pingo.go
+++ cmd/server/pingo.go
@@ -15,8 +15,8 @@ func main() {
 
 	s.MountRoutes()
 
-	log.Printf("Server running on port %s", s.Config.ServerPort)
-	if err := http.ListenAndServe(":"+s.Config.ServerPort, s.Router); err != nil {
+	log.Printf("Server running on port %s", s.Cfg.ServerPort)
+	if err := http.ListenAndServe(":"+s.Cfg.ServerPort, s.Rtr); err != nil {
 		log.Fatal("Server failed to start: ", err)
 	}
 }
