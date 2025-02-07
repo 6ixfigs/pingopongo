1f0b35f2888eaea84d6f2d0a072ffa67a303cdbe
Marcel Muslija <marcel.muslija@gmail.com>
2025-02-07 21:34:17 +0100

    Implement logging; Deny arbitrary json payload in webhooks.Notify by injected "

--- cmd/server/pongo.go
+++ cmd/server/pongo.go
@@ -20,13 +20,13 @@ func main() {
 
 	s, err := rest.NewServer()
 	if err != nil {
-		log.Fatal("Server failed to start: ", err)
+		log.Fatal("server failed to start: ", err)
 	}
 
 	s.MountRoutes()
 
-	log.Printf("Server running on port %s", s.Cfg.ServerPort)
+	log.Printf("server running on port %s", s.Cfg.ServerPort)
 	if err := http.ListenAndServe(":"+s.Cfg.ServerPort, s.Rtr); err != nil {
-		log.Fatal("Server failed to start: ", err)
+		log.Fatal("server failed to start: ", err)
 	}
 }
--- internal/rest/rest.go
+++ internal/rest/rest.go
@@ -2,6 +2,8 @@ package rest
 
 import (
 	"database/sql"
+	"log"
+	"net/http"
 
 	"github.com/6ixfigs/pingypongy/internal/config"
 	"github.com/6ixfigs/pingypongy/internal/db"
@@ -38,6 +40,7 @@ func NewServer() (*Server, error) {
 }
 
 func (s *Server) MountRoutes() {
+	s.Rtr.Use(requestLogger)
 	s.Rtr.Use(middleware.Recoverer)
 	s.Rtr.Use(middleware.CleanPath)
 	s.Rtr.Use(middleware.RedirectSlashes)
@@ -60,4 +63,11 @@ func (s *Server) MountRoutes() {
 	s.Rtr.Mount("/leaderboards/{leaderboard_name}/webhooks", wh.Rtr)
 	s.Rtr.Mount("/leaderboards/{leaderboard_name}/players", ph.Rtr)
 	s.Rtr.Mount("/leaderboards/{leaderboard_name}/matches", mh.Rtr)
+}
+
+func requestLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("HTTP %s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
 }
