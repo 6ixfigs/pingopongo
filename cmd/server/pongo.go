593f9a1cb0ea80ac32ebd0d5990c495e8ae35e6b
Marcel Muslija <marcel.muslija@gmail.com>
2025-02-07 20:45:40 +0100

    Set logger destination to file

--- cmd/server/pongo.go
+++ cmd/server/pongo.go
@@ -3,11 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/6ixfigs/pingypongy/internal/rest"
 )
 
 func main() {
+	logfile, err := os.OpenFile("/var/log/pongo/pongo.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatalf("error opening log file: %v", err)
+	}
+	defer logfile.Close()
+
+	log.SetOutput(logfile)
+	log.SetFlags(log.LstdFlags | log.Lshortfile | log.Ltime)
+
 	s, err := rest.NewServer()
 	if err != nil {
 		log.Fatal("Server failed to start: ", err)
